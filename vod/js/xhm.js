a4bdc7a0752e3c243589eaed2369863b493e4ad9
wangjian410 <84273586+wangjian410@users.noreply.github.com>
2025-02-19 16:00:43 +0800

    Update xhm.js

--- vod/js/xhm.js
+++ vod/js/xhm.js
@@ -5,71 +5,39 @@ import {} from '../../core/uz3lib.js';
 import {} from '../../core/uzUtils.js';
 // ignore
 
-// 类名要特殊
-class ChangZhang20240614 extends WebApiBase {
+class XHamsterAPI extends WebApiBase {
   constructor() {
     super();
-    this.host = 'https://xhamster.com';
-    this.webSite = 'https://xhamster.com';
+    this.host = 'https://zh.xhamster.com/x-api'; // API 请求地址
+    this.webSite = 'https://zh.xhamster.com'; // 网站首页
     this.headers = {
-      'user-agent':
+      'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+      'Content-Type': 'application/json',
+      'Accept': '*/*',
+      'Referer': this.webSite,
+      'X-Requested-With': 'XMLHttpRequest',
     };
   }
 
-  async getNewSiteDomain() {
-    try {
-      const res = await req(this.host, { headers: this.headers });
-      const $ = cheerio.load(res.data);
-      const url = $('h2').eq(1).find('a').attr('href');
-
-      return url;
-    } catch (e) {
-      return this.webSite;
-    }
-  }
-
   /**
-   * 异步获取分类列表的方法。
+   * 获取分类列表
    * @param {UZArgs} args
    * @returns {Promise<RepVideoClassList>}
    */
   async getClassList(args) {
-    let webUrl = args.url;
-    // let webUrl = await this.getNewSiteDomain()
-
-    // 如果通过首页获取分类的话，可以将对象内部的首页更新
-    this.webSite = UZUtils.removeTrailingSlash(webUrl);
-    var backData = new RepVideoClassList();
+    let backData = new RepVideoClassList();
     try {
-      const pro = await req(webUrl, { headers: this.headers });
-      backData.error = pro.error;
-      let proData = pro.data;
-      if (proData) {
-        var document = parse(proData);
-        var allClass = document.querySelectorAll('ul.submenu_mi > li > a');
-        var list = [];
-        for (let index = 0; index < allClass.length; index++) {
-          const element = allClass[index];
-          var isIgnore = this.isIgnoreClassName(element.text);
-          if (isIgnore) {
-            continue;
-          }
-          var type_name = element.text;
-          var url = element.attributes['href'];
-          url = this.combineUrl(url);
-
-          if (url.length > 0 && type_name.length > 0) {
-            var videoClass = new VideoClass();
-            videoClass.type_id = url;
-            videoClass.type_name = type_name;
-            list.push(videoClass);
-          }
-        }
-        backData.data = list;
-      }
+      // xhamster 没有标准的分类 API，可能需要手动填写分类
+      let categories = [
+        { type_id: 'straight', type_name: '直男' },
+        { type_id: 'gay', type_name: '同性' },
+        { type_id: 'trans', type_name: '变性' },
+        { type_id: 'amateur', type_name: '素人' },
+      ];
+      backData.data = categories;
     } catch (error) {
-      backData.error = '获取分类失败～';
+      backData.error = '获取分类失败';
     }
     return JSON.stringify(backData);
   }
@@ -80,44 +48,33 @@ class ChangZhang20240614 extends WebApiBase {
    * @returns {Promise<RepVideoList>}
    */
   async getVideoList(args) {
-    var listUrl = UZUtils.removeTrailingSlash(args.url) + '/page/' + args.page;
-    var backData = new RepVideoList();
+    let backData = new RepVideoList();
     try {
-      let pro = await req(listUrl, null);
-      backData.error = pro.error;
-      let proData = pro.data;
-      if (proData) {
-        var document = parse(proData);
-        var allVideo = document
-          .querySelector('.bt_img.mi_ne_kd.mrb')
-          .querySelectorAll('ul > li');
-        var videos = [];
-        for (let index = 0; index < allVideo.length; index++) {
-          const element = allVideo[index];
-          var vodUrl = element.querySelector('a')?.attributes['href'] ?? '';
-          var vodPic =
-            element.querySelector('a > img')?.attributes['data-original'] ?? '';
-          var vodName =
-            element.querySelector('a > img')?.attributes['alt'] ?? '';
-          var vodDiJiJi = element.querySelector('div.jidi > span')?.text;
-          var vodHD =
-            element.querySelector('div.hdinfo > span.qb')?.text ??
-            element.querySelector('div.hdinfo > span.furk')?.text;
+      const response = await req(this.host, {
+        method: 'POST',
+        headers: this.headers,
+        body: JSON.stringify({
+          category: args.type_id, // 分类 ID
+          limit: 20,
+          page: args.page || 1,
+        }),
+      });
 
-          var vodDouBan = element.querySelector('div.rating')?.text ?? '';
-
+      let data = response.data;
+      if (data && data.videos) {
+        let videos = [];
+        for (let video of data.videos) {
           let videoDet = new VideoDetail();
-          videoDet.vod_id = vodUrl;
-          videoDet.vod_pic = vodPic;
-          videoDet.vod_name = vodName;
-          videoDet.vod_remarks = vodDiJiJi ?? vodHD;
-          videoDet.vod_douban_score = vodDouBan;
+          videoDet.vod_id = video.id;
+          videoDet.vod_name = video.title;
+          videoDet.vod_pic = video.thumbnail_url;
+          videoDet.vod_remarks = video.duration;
           videos.push(videoDet);
         }
         backData.data = videos;
       }
     } catch (error) {
-      backData.error = '获取列表失败～';
+      backData.error = '获取视频列表失败';
     }
     return JSON.stringify(backData);
   }
@@ -128,204 +85,52 @@ class ChangZhang20240614 extends WebApiBase {
    * @returns {Promise<RepVideoDetail>}
    */
   async getVideoDetail(args) {
-    var backData = new RepVideoDetail();
+    let backData = new RepVideoDetail();
     try {
-      var webUrl = args.url;
-      if (false === webUrl.endsWith('.html')) {
-        webUrl =
-          UZUtils.removeTrailingSlash(this.webSite) +
-          '/movie/' +
-          webUrl +
-          '.html';
-      }
+      const response = await req(this.host, {
+        method: 'POST',
+        headers: this.headers,
+        body: JSON.stringify({
+          video_id: args.vod_id,
+        }),
+      });
 
-      let pro = await req(webUrl, { headers: this.headers });
-      backData.error = pro.error;
-      let proData = pro.data;
-      if (proData) {
-        var document = parse(proData);
-        var vod_pic =
-          document.querySelector('.dyimg.fl > img')?.attributes['src'] ?? '';
-        var vod_name =
-          document.querySelector('div.moviedteail_tt > h1')?.text ?? '';
-        var detList =
-          document.querySelector('.moviedteail_list')?.querySelectorAll('li') ??
-          [];
-        var vod_year = '';
-        var vod_director = '';
-        var vod_actor = '';
-        var vod_area = '';
-        var vod_lang = '';
-        var vod_douban_score = '';
-        var type_name = '';
-
-        for (let index = 0; index < detList.length; index++) {
-          const element = detList[index];
-          if (element.text.includes('年份')) {
-            vod_year = element.text.replace('年份：', '');
-          } else if (element.text.includes('导演')) {
-            vod_director = element.text.replace('导演：', '');
-          } else if (element.text.includes('主演')) {
-            vod_actor = element.text.replace('主演：', '');
-          } else if (element.text.includes('地区')) {
-            vod_area = element.text.replace('地区：', '');
-          } else if (element.text.includes('语言')) {
-            vod_lang = element.text.replace('语言：', '');
-          } else if (element.text.includes('类型')) {
-            type_name = element.text.replace('类型：', '');
-          } else if (element.text.includes('豆瓣')) {
-            vod_douban_score = element.text.replace('豆瓣：', '');
-          }
-        }
-
-        var vod_content = '';
-        var vodBlurbDocument = document.querySelector('.yp_context');
-
-        if (vodBlurbDocument) {
-          vod_content = vodBlurbDocument.text;
-
-          var allP = vodBlurbDocument.querySelectorAll('p');
-
-          for (let index = 0; index < allP.length; index++) {
-            const element = allP[index];
-            vod_content = vod_content + element.text;
-          }
-        }
-
-        var juJiDocment =
-          document.querySelector('.paly_list_btn')?.querySelectorAll('a') ?? [];
-
-        var vod_play_url = '';
-        for (let index = 0; index < juJiDocment.length; index++) {
-          const element = juJiDocment[index];
-
-          vod_play_url += element.text;
-          vod_play_url += '$';
-          vod_play_url += element.attributes['href'];
-          vod_play_url += '#';
-        }
-
-        let detModel = new VideoDetail();
-        detModel.vod_year = vod_year;
-        detModel.type_name = type_name;
-        detModel.vod_director = vod_director;
-        detModel.vod_actor = vod_actor;
-        detModel.vod_area = vod_area;
-        detModel.vod_lang = vod_lang;
-        detModel.vod_douban_score = vod_douban_score;
-        detModel.vod_content = vod_content;
-        detModel.vod_pic = vod_pic;
-        detModel.vod_name = vod_name;
-        detModel.vod_play_url = vod_play_url;
-        detModel.vod_id = webUrl;
-        backData.data = detModel;
+      let data = response.data;
+      if (data && data.video) {
+        let video = data.video;
+        backData.vod_id = video.id;
+        backData.vod_name = video.title;
+        backData.vod_pic = video.thumbnail_url;
+        backData.vod_content = video.description || '暂无简介';
       }
     } catch (error) {
       backData.error = '获取视频详情失败';
     }
-
     return JSON.stringify(backData);
   }
 
   /**
-   * 获取视频的播放地址
+   * 获取视频播放地址
    * @param {UZArgs} args
    * @returns {Promise<RepVideoPlayUrl>}
    */
   async getVideoPlayUrl(args) {
-    var backData = new RepVideoPlayUrl();
+    let backData = new RepVideoPlayUrl();
     try {
-      const pro = await req(args.url, { headers: this.headers });
-      backData.error = pro.error;
-      let proData = pro.data;
+      const response = await req(this.host, {
+        method: 'POST',
+        headers: this.headers,
+        body: JSON.stringify({
+          video_id: args.vod_id,
+        }),
+      });
 
-      if (proData) {
-        var document = parse(proData);
-
-        let jsUrl = document.querySelector('iframe')?.attributes['src'] ?? '';
-        if (jsUrl.length > 0) {
-          let headers = {
-            'user-agent': this.headers['user-agent'],
-          };
-          if (jsUrl.includes('player-v2')) {
-            headers['sec-fetch-dest'] = 'iframe';
-            headers['sec-fetch-mode'] = 'navigate';
-            headers['referer'] = `${this.webSite}/`;
-          }
-          let pro2 = await req(jsUrl, {
-            headers: headers,
-          });
-          pro2.error += pro.error;
-          if (pro2.data) {
-            let root = parse(pro2.data);
-            let scripts = root.querySelectorAll('script');
-            var code1 = '';
-            if (scripts.length - 2 > 0) {
-              code1 = scripts[scripts.length - 2].text;
-
-              if (code1.indexOf('var player') > -1) {
-                let player = code1.match(/var player = "(.*?)"/);
-                let rand = code1.match(/var rand = "(.*?)"/);
-
-                let content = JSON.parse(
-                  this.cryptJs(player[1], 'VFBTzdujpR9FWBhe', rand[1])
-                );
-                backData.data = content['url'];
-                backData.headers = this.headers;
-              } else {
-                let data = code1.split('"data":"')[1].split('"')[0];
-                let code = data.split('').reverse().join('');
-                let temp = '';
-                for (let i = 0x0; i < code.length; i = i + 0x2) {
-                  temp += String.fromCharCode(
-                    parseInt(code[i] + code[i + 0x1], 0x10)
-                  );
-                }
-                let playUrl =
-                  temp.substring(0x0, (temp.length - 0x7) / 0x2) +
-                  temp.substring((temp.length - 0x7) / 0x2 + 0x7);
-                // let path = scripts[scripts.length - 1].attributes["src"];
-                // let host = UZUtils.getHostFromURL(jsUrl);
-                // let pro = await req(host + path, {
-                //   headers: {
-                //     Referer: this.webSite,
-                //     "Sec-Fetch-Dest": "iframe",
-                //     "Sec-Fetch-Mode": "navigate",
-                //   },
-                // });
-
-                // 浏览器里这样执行可以。。。
-                // let c =
-                //   'document[_0x2911("43", "EL@a")](_0x82e421)[_0x2911("44", "vU#R")] =';
-                // var videoHtml = "";
-                // let code2 = pro.data.replace(c, "videoHtml =");
-
-                // var res = eval('var videoHtml = "";' + code1 + code2);
-
-                backData.data = playUrl;
-                backData.headers = this.headers;
-                // backData.error = '这个加密不知道怎么解～'
-              }
-            }
-          }
-        }
-
-        let x =
-          document.querySelectorAll('script:contains(window.wp_nonce)') ?? [];
-        if (x.length > 0) {
-          let code = x[0].text;
-          let group = code.match(/(var.*)eval\((\w*\(\w*\))\)/);
-          const md5 = Crypto;
-          const result = eval(group[1] + group[2]);
-          let url = result.match(/url:.*?['"](.*?)['"]/)[1];
-		  const res = await req(url)
-          const b64Data = Crypto.enc.Base64.stringify(Crypto.enc.Utf8.parse(res.data))
-          backData.data = 'data:application/vnd.apple.mpegurl;base64,' + b64Data
-          //backData.data = url;
-        }
+      let data = response.data;
+      if (data && data.video) {
+        backData.data = data.video.play_url;
       }
     } catch (error) {
-      backData.error = '获取视频播放地址失败';
+      backData.error = '获取播放地址失败';
     }
     return JSON.stringify(backData);
   }
@@ -336,99 +141,36 @@ class ChangZhang20240614 extends WebApiBase {
    * @returns {Promise<RepVideoList>}
    */
   async searchVideo(args) {
-    var backData = new RepVideoList();
-    let url = `https://zh.xhamster.com/search`;
+    let backData = new RepVideoList();
     try {
-      let res = await req(url, { headers: this.headers });
-      let data = res.data;
-      if (data) {
-        let videos = [];
-        let list = data.split('$$$');
-        list.forEach((e) => {
-          let p = e.split('|');
-          let videoDet = new VideoDetail();
-          videoDet.vod_id = p[0];
+      const response = await req(this.host, {
+        method: 'POST',
+        headers: this.headers,
+        body: JSON.stringify({
+          keyword: args.searchWord,
+          limit: 20,
+        }),
+      });
 
-          videoDet.vod_pic = p[2];
-          videoDet.vod_name = p[1];
-          videoDet.vod_remarks = p[3];
+      let data = response.data;
+      if (data && data.videos) {
+        let videos = [];
+        for (let video of data.videos) {
+          let videoDet = new VideoDetail();
+          videoDet.vod_id = video.id;
+          videoDet.vod_name = video.title;
+          videoDet.vod_pic = video.thumbnail_url;
+          videoDet.vod_remarks = video.duration;
           videos.push(videoDet);
-        });
+        }
         backData.data = videos;
       }
-    } catch (e) {
-      backData.error = 'search error';
+    } catch (error) {
+      backData.error = '搜索失败';
     }
-    // let url =
-    //   this.removeTrailingSlash(this.webSite) +
-    //   "/daoyongjiekoshibushiyoubing?q=" +
-    //   args.searchWord +
-    //   "&f=_all&p=" +
-    //   args.page;
-    // let pro = await req(url, {
-    //   headers: {
-    //     "Cookie":
-    //       "cf_clearance=FNTfIrcfhaIjgq31GXM.lheyLTqcDDdOmUG6ci8xZo0-1718372164-1.0.1.1-3Sd9Aat3W4QbdrO8l4t6UF2dCLFjuHFeRVeH6VIXDAsYpitprJkKkESjPbPpendwyIuQMfYHzTqj_EXPeDselw; Hm_lvt_07305e6f6305a01dd93218c7fe6bc9c3=1717259553; Hm_lvt_06341c948291d8e90aac72f9d64905b3=1717259553; Hm_lvt_0653ba1ead8a9aabff96252e70492497=1717259553; myannoun=1",
-    //     "User-Agent":
-    //       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
-    //   },
-    // });
-    // backData.error = pro.error;
-    // let proData = pro.data;
-    // if (proData) {
-    //   var document = parse(proData);
-
-    //   let allVideo =
-    //     document
-    //       .querySelector(".bt_img.mi_ne_kd.search_list")
-    //       ?.querySelectorAll("ul > li") ?? [];
-    // }
     return JSON.stringify(backData);
   }
-
-  ignoreClassName = ['关于', '公告', '官方', '备用', '群', '地址'];
-
-  cryptJs(text, key, iv, type) {
-    let key_value = Crypto.enc.Utf8.parse(key || 'PBfAUnTdMjNDe6pL');
-    let iv_value = Crypto.enc.Utf8.parse(iv || 'sENS6bVbwSfvnXrj');
-    let content;
-    if (type) {
-      content = Crypto.AES.encrypt(text, key_value, {
-        iv: iv_value,
-        mode: Crypto.mode.CBC,
-        padding: Crypto.pad.Pkcs7,
-      });
-    } else {
-      content = Crypto.AES.decrypt(text, key_value, {
-        iv: iv_value,
-        padding: Crypto.pad.Pkcs7,
-      }).toString(Crypto.enc.Utf8);
-    }
-    return content;
-  }
-
-  combineUrl(url) {
-    if (url === undefined) {
-      return '';
-    }
-    if (url.indexOf(this.webSite) !== -1) {
-      return url;
-    }
-    if (url.startsWith('/')) {
-      return this.webSite + url;
-    }
-    return this.webSite + '/' + url;
-  }
-
-  isIgnoreClassName(className) {
-    for (let index = 0; index < this.ignoreClassName.length; index++) {
-      const element = this.ignoreClassName[index];
-      if (className.indexOf(element) !== -1) {
-        return true;
-      }
-    }
-    return false;
-  }
 }
-// json 中 instance 的值，这个名称一定要特殊
+
+// json 中 instance 的值，这个名称要唯一
 var xhm20250219 = new xhm20250219();
